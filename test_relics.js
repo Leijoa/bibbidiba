f8bc764b6a1d521d887aac85aa3d98f6ca8cd211
Leijoa <67219619+Leijoa@users.noreply.github.com>
2026-04-24 03:45:36 +0800

    Delete test_relics.js

--- test_relics.js
+++
@@ -1,9 +0,0 @@
-import { calculateEngineScore } from './js/engine.js';
-let dice = [{val:1},{val:2},{val:3},{val:4},{val:5},{val:6},{val:7},{val:8}];
-
-console.log("No relics:", calculateEngineScore(dice, [], 0, 3).totalBase);
-console.log("small:", calculateEngineScore(dice, ['small'], 0, 3).totalBase);
-console.log("mid:", calculateEngineScore(dice, ['mid'], 0, 3).totalBase);
-console.log("big:", calculateEngineScore(dice, ['big'], 0, 3).totalBase);
-console.log("odd:", calculateEngineScore(dice, ['odd'], 0, 3).totalBase);
-console.log("even:", calculateEngineScore(dice, ['even'], 0, 3).totalBase);
